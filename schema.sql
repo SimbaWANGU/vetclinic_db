8169df18fdeb1fd5a8c4b914d1320dd03aaca4fb
SimbaWANGU <simbawangulu@gmail.com>
2022-07-05 20:47:48 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -7,6 +7,7 @@ CREATE TABLE animals(
 	escape_attempts INT NOT NULL,
 	neutered BOOLEAN NOT NULL,
 	weight_kg DECIMAL NOT NULL,
-	species VARCHAR NOT NULL,
 	PRIMARY KEY(id)
 );
+
+ALTER TABLE animals ADD species varchar;
